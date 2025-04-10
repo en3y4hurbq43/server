31e93d075f494d939a017baef0deb3670a9363be
en3y4hurbq43 <149855460+en3y4hurbq43@users.noreply.github.com>
2025-04-10 13:01:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json()); // Permite recibir JSON en las peticiones
 const db = mysql.createConnection({
   host: process.env.DB_HOST || 'srv523.hstgr.io',
   user: process.env.DB_USER || 'u855233599_axel',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'Axel1234567899$',
   database: process.env.DB_NAME || 'u855233599_reportes_db'
 });
 
